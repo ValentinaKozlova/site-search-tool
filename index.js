f4717df1eaa4b1b4785bf344734c44ca9b758a61
Valentina <v.kozlova13@gmail.com>
2021-03-28 22:37:38 +0300

    Added statistics and did some refactoring

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var extractDomain = require('extract-domain');
 const parse = require('node-html-parser').parse;
 
 async function openUrl(url) {
-    return needle('get',url)
+    return needle('get', url)
         .then((response) => {
             if (response.statusCode == 200) {
                 return parse(response.body)
@@ -20,9 +20,9 @@ function findLinks(html, url) {
     //TODO check that link is valid
     //TODO check relative and absolutes URLs
     links.forEach(link => {
-        const href=link.getAttribute('href')
+        const href = link.getAttribute('href')
         const domain = extractDomain(url)
-        const linkRegExp=new RegExp(`^http(|s):\/\/|(www).${domain}`, 'gi')
+        const linkRegExp = new RegExp(`^http(|s):\/\/|(www).${domain}`, 'gi')
         const isCurrentDomain = linkRegExp.test(href)
         if (isCurrentDomain) {
             linksArr.push(href)
@@ -34,7 +34,7 @@ function findLinks(html, url) {
 async function crawlPage(search_fn, {url, level}) {
     try {
         let html = await openUrl(url);
-        return {url, level, links: findLinks(html, url), result: search_fn(html, url)};
+        return {url, level, links: findLinks(html, url), result: search_fn(html)};
     } catch (e) {
         console.error(e);
         return {url, level, links: [], result: null}
@@ -49,15 +49,11 @@ function mergeTasks(acc, {url, task}) {
     return {urls: [...acc.urls, url], tasks: {...acc.tasks, [url]: task}};
 }
 
-function search(keyword, html, url) {
+function search(keyword, html) {
     const body = html && html.querySelector('body');
     const pageContent = body && body.innerText;
-    if (pageContent) {
-        var searchResults = findSearchKeyword(pageContent, keyword)
-        if (!searchResults || searchResults.length === 0) return
-        console.log(`Found on page ${url}\n`)
-        searchResults.map(keyword => console.log(keyword))
-    }
+    const searchResults = pageContent && findSearchKeyword(pageContent, keyword);
+    return searchResults || [];
 }
 
 function findSearchKeyword(content, keyword, contextLength) {
@@ -67,32 +63,30 @@ function findSearchKeyword(content, keyword, contextLength) {
 }
 
 async function crawl(search_fn, rootUrl, depth = 2) {
-    //TODO add general statistics
-    let stack = [{level: 0, url: rootUrl}];
-    let history = [];
-    let tasks = {};
     let results = [];
 
-    while (stack.length > 0 || Object.keys(tasks).length > 0) {
-        let started = stack
-            .map(startTask.bind(null, search_fn))
-            .reduce(mergeTasks, {urls: [], tasks: {}});
+    let {tasks, urls} = mergeTasks(
+        {tasks: {}, urls: []},
+        startTask(search_fn, {url: rootUrl, level: 0})
+    )
 
-        history = [...history, started.urls];
-        tasks = {...tasks, ...started.tasks}
-
+    while (Object.keys(tasks).length > 0) {
         let {url, links, result, level} = await Promise.race(Object.values(tasks));
 
+        delete tasks[url];
+
         if (level < depth && links) {
-            stack = links
-                .filter(url => !history.includes(url))
-                .map(url => ({level: level + 1, url}));
-        } else {
-            stack = []
+            let started = links
+                .filter(url => !urls.includes(url))
+                .map(url => ({level: level + 1, url}))
+                .map(startTask.bind(null, search_fn))
+                .reduce(mergeTasks, {urls: [], tasks: {}});
+
+            urls = [...urls, started.urls];
+            tasks = {...tasks, ...started.tasks};
         }
 
         results.push({url, result});
-        delete tasks[url];
     }
 
     return results;
@@ -100,6 +94,8 @@ async function crawl(search_fn, rootUrl, depth = 2) {
 
 crawl(search.bind(null, ['expert']), 'https://www.justanswer.com/law/').then((res) => {
     console.log(`Crawled ${res.length}`)
+    const resultsFound = res.reduce((count, {result}) => result.length !== 0? count+1 : count, 0)
+    console.log(`Found ${resultsFound}`)
     fs.writeFileSync('./results.txt', JSON.stringify(res));
 })
 // crawl(search.bind(null, ['code']), 'https://medium.com/')
